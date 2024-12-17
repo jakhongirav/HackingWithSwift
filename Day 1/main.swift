1d0ab750abcaaddcb13f79c90d7c7d0a5f19c001
Jahongir Abdujalilov <jabdujalilov24@fed.idserve.net>
2024-12-17 14:19:18 +0100

    variables, simple data types, str interpolations && collections

---
+++ Day 1/main.swift
@@ -0,0 +1,23 @@
+var score = 85
+var str = "Your score was \(score)"
+
+var result: String = "Your test results are here: \(str)"
+// print(result)
+
+// Example 2: Using a variable to store the result of a calculation
+var num1 = 10
+var num2 = 5
+var sum = num1 + num2
+var sumString = "The sum of \(num1) and \(num2) is \(sum)"
+// print(sumString)
+
+// Example 3: Using a variable to store the result of a boolean operation
+var a = 5
+var b = 10
+var isGreater = a > b
+var comparisonResult = "Is \(a) greater than \(b)? \(isGreater)"
+// print(comparisonResult)
+
+// Unchangable variable
+let jakhongir: String = "swift"
+// jakhongir = "java" // This line will cause a compile-time error
