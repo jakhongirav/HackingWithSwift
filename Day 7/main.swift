55fabc0f142b309f3cfc33fa1e60ab42c322b8e1
Jahongir Abdujalilov <jabdujalilov24@fed.idserve.net>
2025-01-09 07:55:38 +0100

    Struct initializers

--- Day 7/main.swift
+++ Day 7/main.swift
@@ -85,3 +85,82 @@ toys.append("Buzz")
 // print(toys.count)  // 2
 // print(toys[1])  // Buzz
 // print(toys.firstIndex(of: "Buzz"))
+
+//* Initializers
+struct User {
+    var username: String
+
+    init() {
+        self.username = "Anonymous"
+        // print("Creating a new user!")
+    }
+}
+
+var user = User()
+user.username = "twostraws"
+// print(user)  // twostraws
+var user1 = User()
+// print(user1)  // Anonymous
+
+//? Referring to the current instance
+struct Person1 {
+    var name: String
+
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+//? Lazy properties
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+
+struct Person2 {
+    var name: String
+    //! lazy keyword to make familyTree independent
+    lazy var familyTree = FamilyTree()
+
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var ed = Person2(name: "Ed")
+//! To be family tree initialized it should be called at least once
+// ed.familyTree
+
+//? Static properties and methods
+struct Student {
+    static var classSize: Int = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+
+var ed1 = Student(name: "Ed")
+var taylor = Student(name: "Taylor")
+// print(Student.classSize)
+
+//? Access control
+struct Person3 {
+    //! to make id not accessible, now only method inside Person3 can readi the id property
+    private var id: String
+
+    //! another common option is public, to let all other code use the property or method
+    // public var id: String
+
+    init(id: String) {
+        self.id = id
+    }
+
+    func identify() -> String {
+        return "My social security number is \(id)"
+    }
+}
