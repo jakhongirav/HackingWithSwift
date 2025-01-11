a43a364b12e210ecfc718ebfd1ed5f148cdcc626
Jahongir Abdujalilov <jabdujalilov24@fed.idserve.net>
2025-01-11 17:07:30 +0100

    creating classes

--- Day 8/main.swift
+++ Day 8/main.swift
@@ -1,1 +1,13 @@
 //* Creating your own classes
+
+class Dog {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+let poppy = Dog(name: "Poppy", breed: "Poodle")
