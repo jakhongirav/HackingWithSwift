50b08d185842003dccbd9c7a18dfffc81613f456
Jahongir Abdujalilov <jabdujalilov24@fed.idserve.net>
2025-01-27 17:37:14 +0100

    WeSplit overview

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jahongir Abdujalilov on 27/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
