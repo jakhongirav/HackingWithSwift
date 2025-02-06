9e57bb949bd7906f5e830e9eec5b7ce8ab058635
Jahongir Abdujalilov <jabdujalilov24@fed.idserve.net>
2025-02-06 22:09:51 +0100

    GuessTheFlag && Intro && Assets

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jahongir Abdujalilov on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
